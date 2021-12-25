a063c80f5e5f40020233d3ad9d9a8f0e862caeda
AkshatRana <akshat.xworkz@gmail.com>
2021-12-25 12:11:42 +0530

    String-Args task file is uploaded. Task no. 1

---
+++ StringArgs.java
@@ -0,0 +1,9 @@
+class StringArgs{
+	public static void main(String[] carName){
+		System.out.println("Welcome to the Showcase of car list");
+		for(int carModel= 0; carModel < carName.length; carModel++){
+				System.out.println(carName[carModel]);
+		}
+		System.out.println("Thank You!!");
+	}	
+}
